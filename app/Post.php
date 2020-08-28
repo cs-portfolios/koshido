53de719fa582077d0d6800fb00069f437025311a
コスト <kosuto@koshidolocallocal.local>
2020-08-28 20:32:37 +0900

    typo

--- app/Post.php
+++ app/Post.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ["title", "name", "user_id"];
-    protected $table = 'post';
-    
+    protected $table = 'posts';
+
     public function comments()
     {
         return $this->hasMany('App\Comment');
