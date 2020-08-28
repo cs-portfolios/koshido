f4fa6fe16ff61d6465fad2377be707894f818a17
コスト <kosuto@koshidolocallocal.local>
2020-08-28 20:19:40 +0900

    テーブル名を指定する

--- app/Post.php
+++ app/Post.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ["title", "name", "user_id"];
-
+    protected $table = 'post';
+    
     public function comments()
     {
         return $this->hasMany('App\Comment');
